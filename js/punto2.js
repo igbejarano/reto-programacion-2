d7c01b4da2a33f5f5c874646210b84ff99bef0f1
igbejarano <bejaranogissel21@gmail.com>
2021-12-20 12:48:15 -0300

    punto2

---
+++ js/punto2.js
@@ -0,0 +1,7 @@
+const nota1 = parseInt(prompt("Ingrese un numero"));
+const nota2 = parseInt(prompt("Ingrese un numero"));
+const nota3 = parseInt(prompt("Ingrese un numero"));
+
+if ((nota1, nota2, nota3 <= 7)) {
+  alert(`Promocionado`);
+}
